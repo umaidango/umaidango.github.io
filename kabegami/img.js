d7499b6f76f6f967c4564a227f7792ca96e44ecc
umaidango <162565401+umaidango@users.noreply.github.com>
2024-08-04 20:44:45 +0900

    Add files via upload

---
+++ kabegami/img.js
@@ -0,0 +1,37 @@
+
+document.getElementById("fgjd0").style.display = "none";
+document.getElementById("fgjd1").style.display = "inline-block";
+document.getElementById("fgjd2").style.display = "inline-block";
+
+setTimeout(function() {
+    na1.innerHTML = '<img class="uwa" src="https://umaidango.github.io/kabegami/index_files/no-10-1.png" style="width: 307px;height: 164px;margin-left: -10px;/*! display: none; */">';
+    na2.innerHTML = '<img class="uwa" src="https://umaidango.github.io/kabegami/index_files/purin-3.png" style="height: 165px;width: 295px;margin-left: -2px;">';
+    na3.innerHTML = '<img class="uwa" src="https://umaidango.github.io/kabegami/index_files/no-10-2.jpg" style="width: 307px;height: 164px;margin-left: -10px;/*! display: none; */">';
+
+  
+  }, 50)
+  setTimeout(function() {
+  ads1.innerHTML = '<img class="img" width="50" height="50" src="https://umaidango.github.io/commons/index_files/images/upl1.jpg" style="height: 85px;width: 182px;margin-top: 0px;">';
+  ads2.innerHTML = '<img class="img" width="50" height="50" src="https://umaidango.github.io/images/coejflkoao.png" style="height: 85px;width: 182px;margin-top: 0px;">';
+  k1c.innerHTML = '<img class="img" width="50" height="50" src="https://umaidango.github.io/kabegami/index_files/umaidango-15.png">';
+  k2c.innerHTML = '<img class="img" width="50" height="50" src="https://umaidango.github.io/kabegami/index_files/obu-2.jpg">';
+  k3c.innerHTML = '<img class="img" width="50" height="50" src="https://umaidango.github.io/kabegami/index_files/purin-2.png">';
+  k4c.innerHTML = '<img class="img" width="50" height="50" src="https://umaidango.github.io/kabegami/index_files/obu-3.jpg">';
+  k5c.innerHTML = '<img class="img" width="50" height="50" src="https://umaidango.github.io/kabegami/index_files/down00003.png">';
+  k6c.innerHTML = '<img class="img" width="50" height="50" src="https://umaidango.github.io/kabegami/index_files/obu-1.jpg" style="width: 180px;">';
+  k7c.innerHTML = '<img class="img" width="80" height="50" src="https://umaidango.github.io/kabegami-download-ver.2/index_files/user-1006-1.png">';
+  k8c.innerHTML = '<img class="img" width="80" height="50" src="https://umaidango.github.io/kabegami-download-ver.2/index_files/umaidago-0009.JPG">';
+  k9c.innerHTML = '<img class="img" width="50" height="50" src="https://umaidango.github.io/kabegami/index_files/down00001.png">';
+  k10c.innerHTML = '<img class="img" width="80" height="50" src="https://umaidango.github.io/kabegami-download-ver.2/index_files/umaidango0013.JPG">';
+  k11c.innerHTML = '<img class="img" width="80" height="50" src="https://umaidango.github.io/kabegami/index_files/down00008.png">';
+  //ここから
+  k12c.innerHTML = '<img class="img" width="50" height="50" src="https://umaidango.github.io/kabegami/index_files/down00000.png">';
+  k13c.innerHTML = '<img class="img" width="50" height="50" src="https://umaidango.github.io/kabegami/index_files/down00002.png">';
+  k14c.innerHTML = '<img class="img" src="https://umaidango.github.io/kabegami/index_files/purin-0001.png">'
+  k15c.innerHTML = '<img class="img" width="50" height="50" src="https://umaidango.github.io/kabegami/index_files/down00005.png">';
+  k16c.innerHTML = '<img class="img" width="80" height="50" src="https://umaidango.github.io/kabegami-download-ver.2/index_files/umaidango0014.JPG">'
+  k17c.innerHTML = '<img class="img" width="50" height="50" src="https://umaidango.github.io/kabegami/index_files/down00004.png">';
+  k18c.innerHTML = '<img class="img" width="80" height="50" src="https://umaidango.github.io/kabegami-download-ver.2/index_files/umaidango0012.JPG">';
+}, 100)
+
+
