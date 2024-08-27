2238441c0c1416956823aa17bc62f5f297b9065c
umaidango <162565401+umaidango@users.noreply.github.com>
2024-08-27 17:24:13 +0900

    Add files via upload

---
+++ kabegami/account-script.js
@@ -0,0 +1,29 @@
+function OpenPurin_a(){
+    document.getElementById("user_purin").style.display = "block";
+
+}
+
+function ClosePurin_a(){
+    document.getElementById("user_purin").style.display = "none";
+
+}
+
+function OpenUser_a(){
+    document.getElementById("user_chipi").style.display = "block";
+
+}
+
+function CloseUser_a(){
+    document.getElementById("user_chipi").style.display = "none";
+
+}
+
+function OpenObu_a(){
+    document.getElementById("user_obu").style.display = "block";
+
+}
+
+function CloseObu_a(){
+    document.getElementById("user_obu").style.display = "none";
+
+}
