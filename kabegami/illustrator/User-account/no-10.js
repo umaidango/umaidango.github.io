e1843a4f8937410927356f505587fb638c576f3f
umaidango <162565401+umaidango@users.noreply.github.com>
2024-08-28 15:44:40 +0900

    Add files via upload

---
+++ kabegami/illustrator/User-account/no-10.js
@@ -0,0 +1,5 @@
+others_sns.innerHTML = "";
+icon_img.innerHTML = '<img height="150" style="border-radius:50%" src="https://umaidango.github.io/kabegami/illustrator/User-account/user-icons/no-10-v1.png">';
+user.innerHTML = 'No.10';
+text0.innerHTML = '説明なし';
+user_name.innerHTML = 'No.10 - Kabegami Download';
---
+++ kabegami/illustrator/User-account/purin.js
@@ -0,0 +1,5 @@
+others_sns.innerHTML = "";
+icon_img.innerHTML = '<img src="https://umaidango.github.io/kabegami-download-ver.2/illustrator/User-account/user-icons/purin-icon-v1.png" width="124" height="124" style="margin-top: 8px;">';
+user.innerHTML = 'ぷりんさん';
+text0.innerHTML = '説明なし';
+user_name.innerHTML = 'ぷりんさん - Kabegami Download';
