4a96254c5512c409788531ad94c44c6d6ae1c66d
umaidango <162565401+umaidango@users.noreply.github.com>
2024-09-07 09:36:08 +0900

    Create kaito.js

---
+++ kabegami/illustrator/User-account/kaito.js
@@ -0,0 +1,5 @@
+others_sns.innerHTML = '<a style="display: inline-block;" href="https://www.tiktok.com/@kaito.hazuma"><svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-tiktok" viewBox="0 0 16 16" style="height: 28px;border: 1px #eee solid;background: #fff;padding: 8px;border-radius: 4px;margin-top: -127px;margin-left: -494px;">   <path d="M9 0h1.98c.144.715.54 1.617 1.235 2.512C12.895 3.389 13.797 4 15 4v2c-1.753 0-3.07-.814-4-1.829V11a5 5 0 1 1-5-5v2a3 3 0 1 0 3 3V0Z"></path> </svg></a>';
+icon_img.innerHTML = '<img height="150" style="border-radius:50%" src="https://umaidango.github.io/kabegami/illustrator/User-account/user-icons/user-icon.png">';
+user.innerHTML = 'かいと';
+text0.innerHTML = '';
+user_name.innerHTML = 'かいと - Kabegami Download';
