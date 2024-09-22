c944a39f227219f0d901fc1264e9365949c9682b
umaidango <162565401+umaidango@users.noreply.github.com>
2024-09-22 19:02:07 +0900

    Create posts.js

---
+++ blog/posts.js
@@ -0,0 +1,1 @@
+posts.innerHTML = '<ul><a href="https://umaidango.github.io/blog/kiji0002.html"><li>MY Widgetのサービス終了に伴い、後継のChipi CHA!について</li></a><a href="https://umaidango.github.io/blog/kiji0001.html"><li>新しいブログサイトができました。</li></a></ul>';
