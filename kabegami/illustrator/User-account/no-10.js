162fa4f656a46d825ddbf734016d8af5d80d63ff
うまいだんご <162565401+umaidango@users.noreply.github.com>
2024-10-25 07:35:27 +0900

    Update no-10.js

--- kabegami/illustrator/User-account/no-10.js
+++ kabegami/illustrator/User-account/no-10.js
@@ -1,4 +1,4 @@
-others_sns.innerHTML = "";
+others_sns.innerHTML = "<a href="https://x.com/IkNkzNRW" style="display: inline-block;"><i class="fa fa-twitter" style="font-size:36px;color: #009fff;background: #fff;border-radius: 4px;padding: 4px;border: 1px #eee solid;margin-top: -138px;margin-left: -498px;"></i></a>";
 icon_img.innerHTML = '<img height="150" style="border-radius:50%" src="https://umaidango.github.io/kabegami/illustrator/User-account/user-icons/no-10-v1.png">';
 user.innerHTML = 'No.10';
 text0.innerHTML = '説明なし';
