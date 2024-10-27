8a246c2ca7d3c6fd4128f99eed27f004b8c29b7b
umaidango <162565401+umaidango@users.noreply.github.com>
2024-10-27 15:04:01 +0900

    f

---
+++ dousa.js
@@ -0,0 +1,17 @@
+
+
+// 要素を取得
+const element = document.getElementById('show_product_menu');
+// 要素を取得
+const element2 = document.getElementById('product_menu');
+
+// ホバーイベントの追加
+element.addEventListener('mouseover', function() {
+    element2.style.display = 'block';
+});
+
+
+// ホバーイベントの追加
+element.addEventListener('mouseout', function() {
+    element2.style.display = 'none';
+});
--- me/main.js
+++ me/main.js
@@ -34,4 +34,3 @@ function menu_close(){
 
 
   
-  
