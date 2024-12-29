0dc283eacce027ee485896c040cb4cef3d755d7a
うまいだんご <162565401+umaidango@users.noreply.github.com>
2024-12-29 10:28:47 +0900

    a

--- me/styles.js
+++ me/styles.js
@@ -8,5 +8,5 @@ setTimeout(load_blog_content, 1000);
 var slide_image = function() {
 let slider = 10;
 picup_img.style ="transform: translate3d(" + "slider" + "px, 0px, 0px); transition:transform 1100ms;";
-  
+}
 setInterval(slide_image, 800);
