8587489d081941bb0ad793eb4261eb8fbb9d7699
うまいだんご <162565401+umaidango@users.noreply.github.com>
2024-12-29 10:28:04 +0900

    a

--- me/styles.js
+++ me/styles.js
@@ -4,3 +4,9 @@ blog.innerHTML = '<iframe style="border: none;width: 100%;overflow: visible;heig
 }
 setTimeout(load_blog_content, 1000);
 
+
+var slide_image = function() {
+let slider = 10;
+picup_img.style ="transform: translate3d(" + "slider" + "px, 0px, 0px); transition:transform 1100ms;";
+  
+setInterval(slide_image, 800);
